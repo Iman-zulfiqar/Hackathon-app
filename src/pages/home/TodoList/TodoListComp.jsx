b4d222d4b5799c9399b0c47c0e1bad2e85977574
Iman-zulfiqar <123621802+Iman-zulfiqar@users.noreply.github.com>
2023-08-06 11:24:38 +0500

    commit

--- src/pages/home/TodoList/TodoListComp.jsx
+++ src/pages/home/TodoList/TodoListComp.jsx
@@ -1,11 +1,9 @@
-// import { useState, useEffect } from "react";
+
 import ListItem from "../../../components/list/ListItem";
 import MenuDropdown from "../../../components/list/MenuDropdown";
-// import { getData } from "../../../apis";
+
 function TodoListComp({ListData}) {
 
-  // const [sortedData, setSortedData]=useState([]);
-  // const [filteredData,setfilteredData]= useState([]);
   const items = [
     {
       label: <a href="https://www.antgroup.com">Delete</a>,
@@ -19,30 +17,7 @@ function TodoListComp({ListData}) {
     },
     
   ];
-//   let  priorities = 
-//   {
-//     'urgent' : 1, 
-//     'critical' : 0,
-//     'normal' : 2,
-  
-//   }
-//   const ListData = (searchValue != '') ? filteredData : sortedData
-//   function customSort (task1, task2) 
-//      {
-//       return priorities[task1.priority] - priorities[task2.priority];
-//   }
-// useEffect(()=>{
-//  if (searchValue != '') {
-//  const data= sortedData.filter((item)=>{
-//     return  item.title.includes(searchValue) || item.description.includes(searchValue)
-//   })
-//   setfilteredData(data);
-//   }
-// },[searchValue])
- 
-//    useEffect(()=>{
-//      getData({setSortedData,customSort})
-//    },[])
+
   return (
     <div className="w-[80%] bg-[#d3d3d3] rounded-2xl mx-auto gap-x-4 flex flex-wrap p-8 shadow-md gap-y-2" style={{height:'70vh'}}>
        {ListData.length !=0 ? ListData.map((item, index)=>{
